c5c1a4f2da5c253ca26e3d3c15ff32ed0d1a24a0
Philliph Drummond <oilsinwater@gmail.com>
2020-02-13 14:31:55 -0800

    fix: moves declarations into AddUserForm

--- src/forms/AddUserForm.js
+++ src/forms/AddUserForm.js
@@ -1,19 +1,22 @@
 import React, { useState } from 'react'
 
-// makes some temporary initial state
-const initialFormState = { id: null, name: '', username: '' }
-const [user, setUser] = useState(initialFormState)
 
-const handleInputChange = event => {
-  // creates function to update the state within the form
-  const { name, value } = event.target
-    // even always passed through to any 'on' event in the DOM
-
-  setUser({ ...user, [name]: value })
-    // using computed property names to dynamically set the name (using [name]) and value
-}
 
 const AddUserForm = props => {
+
+  // makes some temporary initial state
+  const initialFormState = { id: null, name: '', username: '' }
+  const [user, setUser] = useState(initialFormState)
+
+  const handleInputChange = event => {
+    // creates function to update the state within the form
+    const { name, value } = event.target
+    // even always passed through to any 'on' event in the DOM
+
+    setUser({ ...user, [name]: value })
+    // using computed property names to dynamically set the name (using [name]) and value
+  }
+
   return (
     <form>
       <label>Name</label>
