94feab32f0b0cc33447fb71f9281e305fda5467e
Philliph Drummond <oilsinwater@gmail.com>
2020-02-13 14:07:20 -0800

    feat: Adds AddUserForm to create new users

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 // brings in useState import
 import React, { useState } from 'react'
 import UserTable from './tables/UserTable'
+import AddUserForm from './forms/AddUserForm'
 
 const App = () => {
   // dummy data
@@ -13,7 +14,7 @@ const App = () => {
   const [users, setUsers] = useState(usersData)
 
   const addUser = user => {
-      // function to manually take user object as param, and add them users Array of objects
+    // function to manually take user object as param, and add them users Array of objects
     user.id = users.length + 1
     // "...users" code ensures that all the previous users remain in the array.
     setUsers([...users, user])
@@ -25,6 +26,7 @@ const App = () => {
       <div className="flex-row">
         <div className="flex-large">
           <h2>Add user</h2>
+          <AddUserForm addUser={addUser} />
         </div>
         <div className="flex-large">
           <h2>View users</h2>
