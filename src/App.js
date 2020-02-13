b6b634f45302aee24f98d98c93d75591e415dcf6
Philliph Drummond <oilsinwater@gmail.com>
2020-02-13 12:28:47 -0800

    feat: adds functional component instead of class

--- src/App.js
+++ src/App.js
@@ -1,5 +1,19 @@
 import React from 'react'
 
 const App = () => {
-
+  return (
+    <div className="container">
+      <h1>CRUD app with React hooks</h1>
+      <div className="flex-row">
+        <div className="flex-large">
+          <h2>Add User</h2>
+        </div>
+        <div className="flex-large">
+          <h2>View Users</h2>
+        </div>
+      </div>
+    </div>
+  )
 }
+
+export default App
