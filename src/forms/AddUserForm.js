b66373333f5a481b0ced726967265d560d810f34
Philliph Drummond <oilsinwater@gmail.com>
2020-02-13 14:22:46 -0800

    feat: updates AddUserForm with handler function

--- src/forms/AddUserForm.js
+++ src/forms/AddUserForm.js
@@ -1,4 +1,31 @@
 import React, { useState } from 'react'
 
+// makes some temporary initial state
+const initialFormState = {
+  id: null,
+  name: '',
+  username: ''
+}
+const [user, setUser] = useState(initialFormState)
+const handleInputChange = event => {
+  // creates function to update the state within the form
+  const { name, value } = event.target
+  // even always passed through to any 'on' event in the DOM
+
+  setUser({ ...user, [name]: value })
+  // using computed property names to dynamically set the name (using [name]) and value
+}
+
+const AddUserForm = props => {
+  return (
+    <form>
+      <label>Name</label>
+      <input type="text" name="name" value="" />
+      <label>Username</label>
+      <input type="text" name="username" value="" />
+      <button>Add new user</button>
+    </form>
+  )
+}
 
 export default AddUserForm
