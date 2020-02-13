d701ab31a56fd286d2f00a14ecd6249053ddd7ac
Philliph Drummond <oilsinwater@gmail.com>
2020-02-13 14:29:34 -0800

    bug: breaking rules of react hooks

--- src/forms/AddUserForm.js
+++ src/forms/AddUserForm.js
@@ -1,19 +1,16 @@
 import React, { useState } from 'react'
 
 // makes some temporary initial state
-const initialFormState = {
-  id: null,
-  name: '',
-  username: ''
-}
+const initialFormState = { id: null, name: '', username: '' }
 const [user, setUser] = useState(initialFormState)
+
 const handleInputChange = event => {
   // creates function to update the state within the form
   const { name, value } = event.target
-  // even always passed through to any 'on' event in the DOM
+    // even always passed through to any 'on' event in the DOM
 
   setUser({ ...user, [name]: value })
-  // using computed property names to dynamically set the name (using [name]) and value
+    // using computed property names to dynamically set the name (using [name]) and value
 }
 
 const AddUserForm = props => {
