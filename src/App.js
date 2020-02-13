1705b767a9ffb55cafa481f9fd9ba6935bbee5e8
Philliph Drummond <oilsinwater@gmail.com>
2020-02-13 15:16:20 -0800

    feat: sets up switch for edit mode, and a button that will pass the current user into state

--- src/App.js
+++ src/App.js
@@ -12,7 +12,12 @@ const App = () => {
   ]
 
   const [users, setUsers] = useState(usersData)
-
+  const [editing, setEditing] = useState(false)
+  // makes state for whether or not edit mode is turned on
+  const initialFormState = { id: null, name: '', username: '' }
+  // creates some initial empty state for the form
+  const [currentUser, setCurrentUser] = useState(initialFormState)
+  // applies params to see and update current user being edited, with empty user applied to a currentUser state
   const addUser = user => {
     // function to manually take user object as param, and add them users Array of objects
     user.id = users.length + 1
@@ -20,29 +25,38 @@ const App = () => {
     setUsers([...users, user])
   }
 
-  const deleteUser = id => {
-    setUsers(users.filter(user => user.id !== id))
-  }
+  const editRow = user => { 
+    // when "Edit" button is selected on a user, it should return the Edit mode, and set the currentUser
+    setEditing(true)
 
-  return (
-    <div className="container">
-      <h1>CRUD App with Hooks</h1>
-      <div className="flex-row">
-        <div className="flex-large">
-          <h2>Add user</h2>
-          <AddUserForm addUser={addUser} />
-        </div>
-        <div className="flex-large">
-          <h2>View users</h2>
-          <UserTable users={users
+  setCurrentUser({ id: user.id, name: user.name, username: user.username })
+}
+
+const deleteUser = id => {
+  setUsers(users.filter(user => user.id !== id))
+}
+
+return (
+  <div className="container">
+    <h1>CRUD App with Hooks</h1>
+    <div className="flex-row">
+      <div className="flex-large">
+        <h2>Add user</h2>
+        <AddUserForm addUser={addUser} />
+      </div>
+      <div className="flex-large">
+        <h2>View users</h2>
+        <UserTable users={users
           // passing users variable through props
+        } editRow={editRow
+          // passing editRow function through props
         } deleteUser={deleteUser
           // passing deleteUser function through props
-                } />
-        </div>
+        } />
       </div>
     </div>
-  )
+  </div>
+)
 }
 
 export default App
--- src/tables/UserTable.js
+++ src/tables/UserTable.js
@@ -17,7 +17,11 @@ const UserTable = props => (
               <td>{user.name}</td>
               <td>{user.username}</td>
               <td>
-                <button className="button muted-button">Edit</button>
+                <button onClick={() => {
+                  props.editRow(user)
+                }
+                  // adding editRow function to the button
+                } className="button muted-button">Edit</button>
                 <button onClick={() => props.deleteUser(user.id)
                   // adding deleteUser function to the button
                 } className="button muted-button">Delete</button>
